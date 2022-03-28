4f072939524c364ee176295b37effcd534e0a63f
CHCheshire <pringl31@gmail.com>
2022-03-28 14:34:45 +0000

    Add Javascript for taking a turn in N&C

--- assets/js/noughts-and-crosses.js
+++ assets/js/noughts-and-crosses.js
@@ -1,1 +1,28 @@
 /* jshint esversion: 8 */
+
+const message = document.querySelector('.message');
+const boxes = document.querySelectorAll('.box');
+const restart = document.querySelector('button');
+
+let icon = 'X'
+
+const toggle = () => {
+    if ( icon === 'X') {
+        icon ='O';
+        message.innerHTML = "<p>Nought's turn</p>";
+    }
+    else {
+        icon = 'X';
+        message.innerHTML = "<p>Cross's turn</p>";
+    }
+}
+
+boxes.forEach( (el) => {el.addEventListener('click',
+    () => {
+        if ( !el.innerHTML ) {
+            el.innerHTML = `<h1>${icon}</h1>`;
+            toggle();
+            checkForWinner();
+        }
+    });
+});
