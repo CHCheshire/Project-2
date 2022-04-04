5ae72bf3257e618f36cbbf39e672250efcaec360
CHCheshire <pringl31@gmail.com>
2022-04-04 19:25:52 +0000

    Add the checks for the choice of rock in R,P,S

--- assets/js/rock-paper-scissors.js
+++ assets/js/rock-paper-scissors.js
@@ -2,42 +2,52 @@
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorButton = document.querySelector('.scissors');
-const playerOptions = [rockButton, paperButton, scissorButton];
-const computerOptions = ['rock', 'paper', 'scissors']
 
+var computerChoice = Math.ceil(Math.random()*3); 
 
-const rockChoice = () => {
-    document.getElementsByClassName("playerChoice").innerHTML ="Rock";
+if (computerChoice < 1) {
+    computerChoice ="rock";
+} else if (1 <= computerChoice <= 2) {
+    computerChoice = "paper";
+} else { 
+    computerChoice = "scissors"
 }
 
-rockButton.addEventListener('click', rockChoice)
-
-function paperChoice() {
-    document.getElementsByClassName("playerChoice").innerHTML = "Paper";
+var playerChoice = () => {
+    if (rockButton.addEventListener("click")) {
+    playerChoice = "rock";
+    game();
+    } else if (paperButton.addEventListener("click")) {
+        playerChoice = "paper";
+        game();
+    } else (scissorButton.addEventListener("click"))
+        playerChoice= "scissors";
+        game();
 }
 
-paperButton.addEventListener("click", paperChoice)
+function game(playerChoice, computerChoice){
 
-const scissorChoice = () => {
-    document.getElementsByClassName("playerChoice").innerHTML ="Scissors";
-}
+    
+        //Check for Rock
+        if (playerChoice === "rock") {  
+            if (computerChoice === "scissors") {
+                playerOption.innerHTML = `<h2>Rock</h2>`
+                computerOption.innerHTML =`<h2>scissors</h2>`
+                message.innerHTML =`<h2>Player wins!</h2>`
+                playerScore();
+            } else if (computerChoice === "paper") {
+                playerOption.innerHTML = `<h2>Rock</h2>`
+                computerOption.innerHTML = `<h2>Paper</h2>`
+                message.innerHTML =`<h2>Computer wins!</h2>`
+                computerScore();
+            } else {
+                playerOption.innerHTML = `<h2>Rock</h2>`
+                computerOption.innerHTML = `<h2>Rock</h2>`
+                message.innerHTML =`<h2>It's a draw!</h2>`
+            }
+        }
 
-scissorButton.addEventListener("click", scissorChoice)
-
-
-// playerOptions.forEach(option => {
-//     option.addEventListener('click', function(){
-//         const choiceNumber = Math.floor(Math.random()*3);
-//         const computerChoice = computerOptions[choiceNumber];
-//     })
-// }) 
-
-// const declareWinner = (player,computer) => {
-
-// }
-
-
-
+    }
 
 
 function playerScore() {
