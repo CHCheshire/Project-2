9ffeff7595b2023eef3d60823059fe87582a2a18
CHCheshire <pringl31@gmail.com>
2022-04-04 16:31:20 +0000

    Add functionality to player options in R,P,S

--- assets/js/rock-paper-scissors.js
+++ assets/js/rock-paper-scissors.js
@@ -1,1 +1,52 @@
 /* jshint esversion: 8 */
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorButton = document.querySelector('.scissors');
+const playerOptions = [rockButton, paperButton, scissorButton];
+const computerOptions = ['rock', 'paper', 'scissors']
+
+
+const rockChoice = () => {
+    document.getElementsByClassName("playerChoice").innerHTML ="Rock";
+}
+
+rockButton.addEventListener('click', rockChoice)
+
+function paperChoice() {
+    document.getElementsByClassName("playerChoice").innerHTML = "Paper";
+}
+
+paperButton.addEventListener("click", paperChoice)
+
+const scissorChoice = () => {
+    document.getElementsByClassName("playerChoice").innerHTML ="Scissors";
+}
+
+scissorButton.addEventListener("click", scissorChoice)
+
+
+// playerOptions.forEach(option => {
+//     option.addEventListener('click', function(){
+//         const choiceNumber = Math.floor(Math.random()*3);
+//         const computerChoice = computerOptions[choiceNumber];
+//     })
+// }) 
+
+// const declareWinner = (player,computer) => {
+
+// }
+
+
+
+
+
+function playerScore() {
+    let oldplayerScore = parseInt(document.getElementById("p-count").innerText);
+    document.getElementById("p-count").innerText = ++oldplayerScore;
+
+} 
+
+function computerScore() {
+    let oldcomputerScore = parseInt(document.getElementById("c-count").innerText);
+    document.getElementById("c-count").innerText = ++oldcomputerScore; 
+}
