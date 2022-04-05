49143b84f6a630b3c508bd65f863e270b7795ed1
CHCheshire <pringl31@gmail.com>
2022-04-05 12:40:15 +0000

    Add function for choosing rock in R,P,S

--- assets/js/rock-paper-scissors.js
+++ assets/js/rock-paper-scissors.js
@@ -1,29 +1,44 @@
 /* jshint esversion: 8 */
-const rockButton = document.querySelector('.rock');
-const paperButton = document.querySelector('.paper');
-const scissorButton = document.querySelector('.scissors');
+const paperButton = document.getElementsByClassName('.paper');
+const scissorButton = document.getElementsByClassName('.scissors');
 
-var computerChoice = Math.ceil(Math.random()*3); 
 
-if (computerChoice < 1) {
-    computerChoice ="rock";
-} else if (1 <= computerChoice <= 2) {
-    computerChoice = "paper";
-} else { 
-    computerChoice = "scissors"
+function computerPlay () {
+
+    var computerChoice = Math.ceil(Math.random()*3); 
+
+    if (computerChoice < 1) {
+        computerChoice ="rock";
+    } else if (1 <= computerChoice <= 2) {
+        computerChoice = "paper";
+    } else { 
+        computerChoice = "scissors"
+    }
 }
 
-var playerChoice = () => {
-    if (rockButton.addEventListener("click")) {
+function rockChoice () {
     playerChoice = "rock";
+    computerPlay();
     game();
-    } else if (paperButton.addEventListener("click")) {
-        playerChoice = "paper";
-        game();
-    } else (scissorButton.addEventListener("click"))
-        playerChoice= "scissors";
-        game();
 }
+
+let rockBtn = document.getElementById('rock');
+rockBtn.addEventListener('click', rockChoice);
+
+// var playerChoice = () => {
+//     if (rockButton.addEventListener("click")) {
+//     playerChoice = "rock";
+//     computerPlay();
+//     game();
+//     } else if (paperButton.addEventListener("click")) {
+//         playerChoice = "paper";
+//         computerPlay();
+//         game();
+//     } else (scissorButton.addEventListener("click"))
+//         playerChoice= "scissors";
+//         computerPlay();
+//         game();
+// }
 
 function game(playerChoice, computerChoice){
 
@@ -38,6 +53,40 @@ function game(playerChoice, computerChoice){
             } else if (computerChoice === "paper") {
                 playerOption.innerHTML = `<h2>Rock</h2>`
                 computerOption.innerHTML = `<h2>Paper</h2>`
+                message.innerHTML =`<h2>Computer wins!</h2>`
+                computerScore();
+            } else {
+                playerOption.innerHTML = `<h2>Rock</h2>`
+                computerOption.innerHTML = `<h2>Rock</h2>`
+                message.innerHTML =`<h2>It's a draw!</h2>`
+            }
+        }
+        if (playerChoice === "paper") {  
+            if (computerChoice === "rock") {
+                playerOption.innerHTML = `<h2>Paper</h2>`
+                computerOption.innerHTML =`<h2>Rock</h2>`
+                message.innerHTML =`<h2>Player wins!</h2>`
+                playerScore();
+            } else if (computerChoice === "scissors") {
+                playerOption.innerHTML = `<h2>Paper</h2>`
+                computerOption.innerHTML = `<h2>Scissors</h2>`
+                message.innerHTML =`<h2>Computer wins!</h2>`
+                computerScore();
+            } else {
+                playerOption.innerHTML = `<h2>Paper</h2>`
+                computerOption.innerHTML = `<h2>Paper</h2>`
+                message.innerHTML =`<h2>It's a draw!</h2>`
+            }
+        }
+        if (playerChoice === "scissors") {  
+            if (computerChoice === "paper") {
+                playerOption.innerHTML = `<h2>Scissors</h2>`
+                computerOption.innerHTML =`<h2>Paper</h2>`
+                message.innerHTML =`<h2>Player wins!</h2>`
+                playerScore();
+            } else if (computerChoice === "rock") {
+                playerOption.innerHTML = `<h2>Scissors</h2>`
+                computerOption.innerHTML = `<h2>Rock</h2>`
                 message.innerHTML =`<h2>Computer wins!</h2>`
                 computerScore();
             } else {
